b9050516419d1785a65ed63426b8836eb8773e6e
Nick Fagerlund <nick.fagerlund@gmail.com>
2025-12-02 10:12:52 -0800

    Oh right, example shouldn't be wrapped to multiline

--- src/day2.rs
+++ src/day2.rs
@@ -5,9 +5,7 @@ pub fn part1(input: &str) -> Result<String, anyhow::Error> {
     Err(anyhow!("lol"))
 }
 
-const _EXAMPLE: &str = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
-1698522-1698528,446443-446449,38593856-38593862,565653-565659,
-824824821-824824827,2121212118-2121212124";
+const _EXAMPLE: &str = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
 
 fn parse_range(txt: &str) -> Result<RangeInclusive<u32>, anyhow::Error> {
     let (first, second) = txt.split_once('-').ok_or(anyhow!("not hyphenated pair"))?;
