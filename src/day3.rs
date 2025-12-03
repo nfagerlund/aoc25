0a2fdb2e5b44e05e711a4985882c21feacc20b93
Nick Fagerlund <nick.fagerlund@gmail.com>
2025-12-03 15:51:25 -0800

    Well, this passes the test but fails the input.

--- src/day3.rs
+++ src/day3.rs
@@ -1,7 +1,15 @@
 use anyhow::anyhow;
 
 pub fn part1(input: &str) -> Result<String, anyhow::Error> {
-    Err(anyhow!("unimplemented"))
+    let mut working_bank = Vec::<u32>::with_capacity(100); // counted the line length.
+    let mut sum = 0_u32;
+    for line in input.lines() {
+        load_bank(&mut working_bank, line);
+        let res = process_bank_part1(&working_bank);
+        println!("res: {res}");
+        sum += res;
+    }
+    Ok(format!("{}", sum))
 }
 
 pub fn part2(input: &str) -> Result<String, anyhow::Error> {
@@ -25,4 +33,37 @@ fn part_2_test() {
         part2(_EXAMPLE).expect("should ok"),
         "aseothuaestnhu".to_string()
     );
+}
+
+/// void: replaces the contents of the provided vec with numbers extracted from
+/// the line of text.
+fn load_bank(dest: &mut Vec<u32>, line: &str) {
+    let stuff = line
+        .chars()
+        .map(|c| c.to_digit(10).expect("malformed input"));
+    dest.clear();
+    dest.extend(stuff);
+}
+
+/// Returns the biggest two-digit number that can be formed from a bank of
+/// digits (respecting order, but with any amount of space between them).
+fn process_bank_part1(bank: &[u32]) -> u32 {
+    // Locate the biggest number prior to the final element of the bank.
+    let (index, &first_digit) = bank[0..(bank.len() - 1)]
+        .iter()
+        .enumerate()
+        .max_by(|a, b| a.1.cmp(b.1))
+        .expect("bank must have at least two digits");
+    let &second_digit = bank[(index + 1)..]
+        .iter()
+        .max()
+        .expect("must be at least one digit left after the first");
+    first_digit * 10 + second_digit
+}
+
+#[test]
+fn slicing_test() {
+    let s = &[0, 1, 2, 3, 4];
+    // well, all right then. good ol off by one.
+    assert_eq!(&s[0..(s.len() - 1)], &[0, 1, 2, 3]);
 }
