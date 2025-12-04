2f068c7d2d0fd1ebf13f43cab09a09b6bc8b5f5c
Nick Fagerlund <nick.fagerlund@gmail.com>
2025-12-03 18:22:04 -0800

    more cleanup...

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,10 @@ mod day1;
 mod day2;
 mod day3;
 
-fn main() {
-    let mut args = std::env::args();
-    args.next();
-    let puzzle = args.next().expect("Requires a puzzle argument, e.g. `1-1`");
-    let f: fn(&str) -> Result<String, anyhow::Error> = match puzzle.as_str() {
+/// Maps puzzle identifiers to implementation functions. The main thing you
+/// gotta add when starting a new day.
+fn puzzle_fn(p: &str) -> fn(&str) -> Result<String, anyhow::Error> {
+    match p {
         "1-1" => day1::part1,
         "1-2" => day1::part2,
         "2-1" => day2::part1,
@@ -14,13 +13,21 @@ fn main() {
         "3-1" => day3::part1,
         "3-2" => day3::part2,
         _ => panic!("That's not a valid puzzle yet"),
-    };
-    let day = puzzle
-        .split('-')
-        .next()
-        .expect("actually that can't ever fail.");
+    }
+}
+
+fn input_string(p: &str) -> String {
+    let day = p.split('-').next().expect("actually that can't ever fail.");
     let input_file = format!("inputs/day{day}.txt");
-    let input = std::fs::read_to_string(input_file).expect("can't find inputs for that");
+    std::fs::read_to_string(input_file).expect("can't find inputs for that")
+}
+
+fn main() {
+    let mut args = std::env::args();
+    args.next();
+    let puzzle = args.next().expect("Requires a puzzle argument, e.g. `1-1`");
+    let f = puzzle_fn(&puzzle);
+    let input = input_string(&puzzle);
     let output = f(&input).expect("implementation returned error");
 
     println!("got output:\n{}", &output);
