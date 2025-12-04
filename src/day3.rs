2ad9d3c2045e6da67bf5977414f72db7cc1da184
Nick Fagerlund <nick.fagerlund@gmail.com>
2025-12-03 16:12:30 -0800

    Implemented part 2! OH NO!! u32::MAX is ten digits long!

--- src/day3.rs
+++ src/day3.rs
@@ -13,7 +13,15 @@ pub fn part1(input: &str) -> Result<String, anyhow::Error> {
 }
 
 pub fn part2(input: &str) -> Result<String, anyhow::Error> {
-    Err(anyhow!("unimplemented"))
+    let mut working_bank = Vec::<u32>::with_capacity(100); // counted the line length.
+    let mut sum = 0_u32;
+    for line in input.lines() {
+        load_bank(&mut working_bank, line);
+        let res = process_bank_part2(&working_bank, 12);
+        println!("res: {res}");
+        sum += res;
+    }
+    Ok(format!("{}", sum))
 }
 
 const _EXAMPLE: &str = "987654321111111
@@ -48,26 +56,41 @@ fn load_bank(dest: &mut Vec<u32>, line: &str) {
 /// Returns the biggest two-digit number that can be formed from a bank of
 /// digits (respecting order, but with any amount of space between them).
 fn process_bank_part1(bank: &[u32]) -> u32 {
-    // Locate the biggest number prior to the final element of the bank. Note
-    // that equal numbers aren't interchangeable; we want the leftmost biggest
-    // one, bc it retains the most agency for the second digit!
-    let (index, &first_digit) = bank[0..(bank.len() - 1)]
-        .iter()
-        .enumerate()
-        .max_by(|a, b| {
-            if a.1 == b.1 {
-                // prefer leftmost, i.e. smallest index
-                b.0.cmp(&a.0)
-            } else {
-                a.1.cmp(b.1)
-            }
-        })
-        .expect("bank must have at least two digits");
-    let &second_digit = bank[(index + 1)..]
-        .iter()
-        .max()
-        .expect("must be at least one digit left after the first");
-    first_digit * 10 + second_digit
+    process_bank_part2(bank, 2)
+}
+
+fn process_bank_part2(bank: &[u32], digits: u32) -> u32 {
+    let mut accumulator = 0_u32;
+    let mut starting_index = 0_usize;
+
+    // fencepost check: for 2 digits, we want 10^0 and 10^1.
+    // work backwards, biggest place first.
+    for i in (0..digits).rev() {
+        // Locate the biggest number that still leaves space to fill the remaining
+        // digits. Note that equal numbers aren't interchangeable; we want the
+        // leftmost biggest one, bc it retains the most agency for the remaining digits!
+
+        // len - i: for the tens digit, i would be 1, matching our original impl.
+        let max_index = bank.len() - (i as usize);
+        let (index, &digit) = bank[starting_index..max_index]
+            .iter()
+            .enumerate()
+            .max_by(|a, b| {
+                if a.1 == b.1 {
+                    // prefer leftmost, i.e. smallest index
+                    b.0.cmp(&a.0)
+                } else {
+                    a.1.cmp(b.1)
+                }
+            })
+            .expect("bank must have at least n digits");
+        // Remember where to start for the next digit
+        starting_index = index + 1;
+        // put it in place
+        accumulator += 10u32.pow(i) * digit;
+    }
+
+    accumulator
 }
 
 #[test]
