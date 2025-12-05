47b50769ebf1a8e01d179433d2cf6ed77f9561ff
Nick Fagerlund <nick.fagerlund@gmail.com>
2025-12-04 22:06:38 -0800

    part 1 test

--- src/dispatch/day5.rs
+++ src/dispatch/day5.rs
@@ -8,11 +8,23 @@ pub fn part2(input: &str) -> Result<String, anyhow::Error> {
     Err(anyhow!("not implemented"))
 }
 
-const _EXAMPLE: &str = "";
+// fresh ranges (inclusive), then available ids
+const _EXAMPLE: &str = "3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32
+";
 
 #[test]
 fn part1_test() {
-    assert_eq!(part1(_EXAMPLE).expect("should ok"), "LOL".to_string());
+    assert_eq!(part1(_EXAMPLE).expect("should ok"), "3".to_string());
 }
 
 #[test]
