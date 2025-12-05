13925d92b88a14be36ffe854f66ddd921dacce5f
Nick Fagerlund <nick.fagerlund@gmail.com>
2025-12-04 22:56:36 -0800

    some helpers :]

--- src/dispatch/day5.rs
+++ src/dispatch/day5.rs
@@ -1,6 +1,6 @@
 use crate::util::parse_range;
 use anyhow::anyhow;
-use std::ops::RangeInclusive;
+use std::{cmp::Ordering, ops::RangeInclusive};
 
 // Count how many ingredient IDs are in at least one fresh range.
 pub fn part1(input: &str) -> Result<String, anyhow::Error> {
@@ -61,3 +61,60 @@ fn parse_inputs(input: &str) -> anyhow::Result<(Vec<RangeInclusive<u64>>, Vec<u6
 
     Ok((ranges, ids))
 }
+
+/// what it sez
+fn cmp_ranges(a: &RangeInclusive<u64>, b: &RangeInclusive<u64>) -> Ordering {
+    match a.start().cmp(b.start()) {
+        Ordering::Less => Ordering::Less,
+        Ordering::Equal => a.end().cmp(b.end()),
+        Ordering::Greater => Ordering::Greater,
+    }
+}
+
+/// only cares about forward overlap, i.e. b is equal to or greater than a. Since we're sorting.
+fn overlaps(a: &RangeInclusive<u64>, b: &RangeInclusive<u64>) -> bool {
+    a.start() <= b.start() && a.end() >= b.start()
+}
+
+/// Combines two ranges into mega-range.
+/// be sure to check with overlaps() first, because we ain't checkin' shit.
+fn merge(a: RangeInclusive<u64>, b: RangeInclusive<u64>) -> RangeInclusive<u64> {
+    let start = *a.start().min(b.start());
+    let end = *a.end().max(b.end());
+    start..=end
+}
+
+#[test]
+fn overlaps_test() {
+    // overlaps but doesn't go further
+    assert!(overlaps(&(0..=9), &(0..=8)));
+    // fully subsumed
+    assert!(overlaps(&(0..=9), &(1..=8)));
+    // goes further
+    assert!(overlaps(&(0..=9), &(5..=12)));
+    // exact handoff
+    assert!(overlaps(&(0..=9), &(9..=15)));
+
+    // disjoint
+    assert!(!overlaps(&(0..=9), &(10..=15)));
+}
+
+#[test]
+fn merge_test() {
+    // [a  ]
+    //    [b  ]
+    assert_eq!(merge(1..=5, 4..=9), 1..=9);
+    // [a  ]
+    // [b    ]
+    assert_eq!(merge(2..=5, 2..=9), 2..=9);
+    // [a     ]
+    //    [b  ]
+    assert_eq!(merge(1..=5, 4..=5), 1..=5);
+    //    [a  ]
+    // [b   ]
+    assert_eq!(merge(4..=9, 1..=5), 1..=9);
+    // [a  ]
+    //      [b  ]
+    assert_eq!(merge(1..=5, 5..=9), 1..=9);
+}
+
