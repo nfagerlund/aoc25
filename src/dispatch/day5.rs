560947e9edabe11fabd3c36c2dbc571c327e5674
Nick Fagerlund <nick.fagerlund@gmail.com>
2025-12-04 22:20:38 -0800

    Gold star 5-1... too easy?

--- src/dispatch/day5.rs
+++ src/dispatch/day5.rs
@@ -1,8 +1,16 @@
 use crate::util::parse_range;
 use anyhow::anyhow;
+use std::ops::RangeInclusive;
 
+// Count how many ingredient IDs are in at least one fresh range.
 pub fn part1(input: &str) -> Result<String, anyhow::Error> {
-    Err(anyhow!("not implemented"))
+    let (ranges, ids) = parse_inputs(input)?;
+    let count = ids
+        .iter()
+        .filter(|&n| ranges.iter().any(|r| r.contains(n)))
+        .count();
+
+    Ok(format!("{count}"))
 }
 
 pub fn part2(input: &str) -> Result<String, anyhow::Error> {
@@ -31,4 +39,23 @@ fn part1_test() {
 #[test]
 fn part2_test() {
     assert_eq!(part2(_EXAMPLE).expect("should ok"), "LOL".to_string());
+}
+
+/// Turn the day5 input string into numeric types
+fn parse_inputs(input: &str) -> anyhow::Result<(Vec<RangeInclusive<u64>>, Vec<u64>)> {
+    let (ranges_str, ids_str) = input
+        .split_once("\n\n")
+        .ok_or(anyhow!("input not a double-newline-separated list pair"))?;
+    // Since this is fallible, I want to use the ? operator to just propagate failure.
+    // But that rules out iterator adaptors, alas, so it's a for loop.
+    let mut ranges = Vec::<RangeInclusive<u64>>::new();
+    for line in ranges_str.lines() {
+        ranges.push(parse_range(line)?);
+    }
+    let mut ids = Vec::<u64>::new();
+    for line in ids_str.lines() {
+        ids.push(line.parse()?);
+    }
+
+    Ok((ranges, ids))
 }
