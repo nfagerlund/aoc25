e5415900af1bc4818d690628d328691bce49871a
Nick Fagerlund <nick.fagerlund@gmail.com>
2025-12-08 08:52:35 -0800

    example string and part 1 test

--- src/dispatch/day8.rs
+++ src/dispatch/day8.rs
@@ -8,11 +8,31 @@ pub fn part2(input: &str) -> Result<String, anyhow::Error> {
     Err(anyhow!("not implemented"))
 }
 
-const _EXAMPLE: &str = "";
+const _EXAMPLE: &str = "162,817,812
+57,618,57
+906,360,560
+592,479,940
+352,342,300
+466,668,158
+542,29,236
+431,825,988
+739,650,466
+52,470,668
+216,146,977
+819,987,18
+117,168,530
+805,96,715
+346,949,466
+970,615,88
+941,993,340
+862,61,35
+984,92,344
+425,690,689
+";
 
 #[test]
 fn part1_test() {
-    assert_eq!(part1(_EXAMPLE).expect("should ok"), "LOL".to_string());
+    assert_eq!(part1(_EXAMPLE).expect("should ok"), "40".to_string());
 }
 
 #[test]
