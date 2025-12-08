ad3ad9f4fd4d9867a3f4c8bd09e6ee6f56644ed4
Nick Fagerlund <nick.fagerlund@gmail.com>
2025-12-08 09:00:47 -0800

    Time to go visit an old friend....

--- src/dispatch/day4.rs
+++ src/dispatch/day4.rs
@@ -1,9 +1,9 @@
-use anyhow::anyhow;
+use crate::util::Grid;
 
 /// Find all *occupied* cells where fewer than four of the eight surrounding
 /// cells are occupied.
 pub fn part1(input: &str) -> Result<String, anyhow::Error> {
-    let grid = Grid::build(input)?;
+    let grid = build_grid(input)?;
     let all_roll_indices = grid.iter_occupied_indices();
     let all_roll_neighbor_counts = all_roll_indices.map(|i| {
         let coords = grid.coords(i);
@@ -16,7 +16,7 @@ pub fn part1(input: &str) -> Result<String, anyhow::Error> {
 /// Iteratively count how many rolls of paper can be removed, according to the
 /// accessibility rules in part 1. Each iteration exposes more rolls.
 pub fn part2(input: &str) -> Result<String, anyhow::Error> {
-    let mut grid = Grid::build(input)?;
+    let mut grid = build_grid(input)?;
     let mut count = 0;
 
     loop {
@@ -52,154 +52,10 @@ fn part2_test() {
     assert_eq!(part2(_EXAMPLE).expect("should ok"), "43".to_string());
 }
 
-// Can I implement a grid without making the underlying array two-dimensional?
-
-/// Zero-indexed coordinates, positive Y is down (like raster graphics).
-struct Grid<T> {
-    storage: Vec<T>,
-    width: usize,
-}
-
-type Coords = (usize, usize);
-
-#[derive(Debug, Clone, Copy, PartialEq)]
-enum Dir {
-    // unit circle order for autocomplete
-    E,
-    NE,
-    N,
-    NW,
-    W,
-    SW,
-    S,
-    SE,
-}
-
-impl Dir {
-    const ALL: &[Self] = &[
-        Self::E,
-        Self::NE,
-        Self::N,
-        Self::NW,
-        Self::W,
-        Self::SW,
-        Self::S,
-        Self::SE,
-    ];
-}
-
-/// positive Y is south. Ignoring grid size.
-fn traverse(coords: Coords, dir: Dir) -> Option<Coords> {
-    let (x, y) = coords;
-    let n = match dir {
-        Dir::E => (x + 1, y),
-        Dir::NE => (x + 1, y.checked_sub(1)?),
-        Dir::N => (x, y.checked_sub(1)?),
-        Dir::NW => (x.checked_sub(1)?, y.checked_sub(1)?),
-        Dir::W => (x.checked_sub(1)?, y),
-        Dir::SW => (x.checked_sub(1)?, y + 1),
-        Dir::S => (x, y + 1),
-        Dir::SE => (x + 1, y + 1),
-    };
-    Some(n)
-}
-
-impl<T> Grid<T> {
-    fn try_new(width: usize, stuff: Vec<T>) -> anyhow::Result<Self> {
-        if !stuff.len().is_multiple_of(width) {
-            Err(anyhow!(
-                "storage length {} is not a multiple of width {}",
-                stuff.len(),
-                width
-            ))
-        } else {
-            Ok(Self {
-                storage: stuff,
-                width,
-            })
-        }
-    }
-
-    /// Returns none if x exceeds width.
-    fn index(&self, coords: Coords) -> Option<usize> {
-        let (x, y) = coords;
-        // zero-indexed, so max x in a 5-width is 4
-        if x >= self.width {
-            None
-        } else {
-            Some(x + (y * self.width))
-        }
-    }
-
-    fn coords(&self, index: usize) -> Coords {
-        let y = index / self.width;
-        let x = index % self.width;
-        (x, y)
-    }
-
-    /// Get the value at a grid cell
-    fn get(&self, coords: Coords) -> Option<&T> {
-        let index = self.index(coords)?;
-        self.storage.get(index)
-    }
-
-    fn _get_by_index(&self, index: usize) -> Option<&T> {
-        self.storage.get(index)
-    }
-
-    fn get_neighbor(&self, coords: Coords, dir: Dir) -> Option<&T> {
-        let neighbor = traverse(coords, dir)?;
-        self.get(neighbor)
-    }
-}
-
-impl Grid<bool> {
-    fn build(ascii: &str) -> anyhow::Result<Self> {
-        let width = width_of_ascii_grid(ascii);
-        let stuff = load_ascii_grid_to_vec_of_bools(ascii);
-        Grid::try_new(width, stuff)
-    }
-
-    fn count_occupied_neighbors(&self, coords: Coords) -> usize {
-        Dir::ALL
-            .iter()
-            .filter_map(|&dir| {
-                let v = self.get_neighbor(coords, dir)?;
-                match v {
-                    true => Some(true),
-                    false => None,
-                }
-            })
-            .count()
-    }
-
-    fn iter_occupied_indices(&self) -> impl Iterator<Item = usize> {
-        self.storage.iter().enumerate().filter_map(
-            |(index, value)| {
-                if *value { Some(index) } else { None }
-            },
-        )
-    }
-
-    // I realized we can just evict as-we-go, because there's no requirement for
-    // instantaneous/simultaneous processing of a given iteration; we just need
-    // to eventually converge, so it's fine if we exploit some slots that opened
-    // up due to actions we just took. Good, even.
-    fn evict_and_count(&mut self) -> usize {
-        let mut count = 0;
-        for i in 0..self.storage.len() {
-            if let Some(occupied) = self.storage.get(i)
-                && *occupied
-            {
-                let coords = self.coords(i);
-                if self.count_occupied_neighbors(coords) < 4 {
-                    count += 1;
-                    self.storage[i] = false;
-                }
-            }
-        }
-        count
-    }
+fn build_grid(ascii: &str) -> anyhow::Result<Grid<bool>> {
+    let width = width_of_ascii_grid(ascii);
+    let stuff = load_ascii_grid_to_vec_of_bools(ascii);
+    Grid::try_new(width, stuff)
 }
 
 fn load_ascii_grid_to_vec_of_bools(input: &str) -> Vec<bool> {
--- src/util.rs
+++ src/util.rs
@@ -8,3 +8,147 @@ pub fn parse_range(txt: &str) -> Result<RangeInclusive<u64>, anyhow::Error> {
 
     Ok(start..=end)
 }
+
+/// A two-dimensional grid, implemented as a single-dimensional array that
+/// translates coordinates <-> indices on the fly. Zero-indexed coordinates,
+/// positive Y is down (like raster graphics).
+pub struct Grid<T> {
+    pub storage: Vec<T>,
+    pub width: usize,
+}
+
+type Coords = (usize, usize);
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Dir {
+    // unit circle order for autocomplete
+    E,
+    NE,
+    N,
+    NW,
+    W,
+    SW,
+    S,
+    SE,
+}
+
+impl Dir {
+    pub const ALL: &[Self] = &[
+        Self::E,
+        Self::NE,
+        Self::N,
+        Self::NW,
+        Self::W,
+        Self::SW,
+        Self::S,
+        Self::SE,
+    ];
+}
+
+/// positive Y is south. Ignoring grid size.
+pub fn traverse(coords: Coords, dir: Dir) -> Option<Coords> {
+    let (x, y) = coords;
+    let n = match dir {
+        Dir::E => (x + 1, y),
+        Dir::NE => (x + 1, y.checked_sub(1)?),
+        Dir::N => (x, y.checked_sub(1)?),
+        Dir::NW => (x.checked_sub(1)?, y.checked_sub(1)?),
+        Dir::W => (x.checked_sub(1)?, y),
+        Dir::SW => (x.checked_sub(1)?, y + 1),
+        Dir::S => (x, y + 1),
+        Dir::SE => (x + 1, y + 1),
+    };
+    Some(n)
+}
+
+impl<T> Grid<T> {
+    pub fn try_new(width: usize, stuff: Vec<T>) -> anyhow::Result<Self> {
+        if !stuff.len().is_multiple_of(width) {
+            Err(anyhow!(
+                "storage length {} is not a multiple of width {}",
+                stuff.len(),
+                width
+            ))
+        } else {
+            Ok(Self {
+                storage: stuff,
+                width,
+            })
+        }
+    }
+
+    /// Returns none if x exceeds width.
+    pub fn index(&self, coords: Coords) -> Option<usize> {
+        let (x, y) = coords;
+        // zero-indexed, so max x in a 5-width is 4
+        if x >= self.width {
+            None
+        } else {
+            Some(x + (y * self.width))
+        }
+    }
+
+    pub fn coords(&self, index: usize) -> Coords {
+        let y = index / self.width;
+        let x = index % self.width;
+        (x, y)
+    }
+
+    /// Get the value at a grid cell
+    pub fn get(&self, coords: Coords) -> Option<&T> {
+        let index = self.index(coords)?;
+        self.storage.get(index)
+    }
+
+    fn _get_by_index(&self, index: usize) -> Option<&T> {
+        self.storage.get(index)
+    }
+
+    pub fn get_neighbor(&self, coords: Coords, dir: Dir) -> Option<&T> {
+        let neighbor = traverse(coords, dir)?;
+        self.get(neighbor)
+    }
+}
+
+impl Grid<bool> {
+    pub fn count_occupied_neighbors(&self, coords: Coords) -> usize {
+        Dir::ALL
+            .iter()
+            .filter_map(|&dir| {
+                let v = self.get_neighbor(coords, dir)?;
+                match v {
+                    true => Some(true),
+                    false => None,
+                }
+            })
+            .count()
+    }
+
+    pub fn iter_occupied_indices(&self) -> impl Iterator<Item = usize> {
+        self.storage.iter().enumerate().filter_map(
+            |(index, value)| {
+                if *value { Some(index) } else { None }
+            },
+        )
+    }
+
+    // I realized we can just evict as-we-go, because there's no requirement for
+    // instantaneous/simultaneous processing of a given iteration; we just need
+    // to eventually converge, so it's fine if we exploit some slots that opened
+    // up due to actions we just took. Good, even.
+    pub fn evict_and_count(&mut self) -> usize {
+        let mut count = 0;
+        for i in 0..self.storage.len() {
+            if let Some(occupied) = self.storage.get(i)
+                && *occupied
+            {
+                let coords = self.coords(i);
+                if self.count_occupied_neighbors(coords) < 4 {
+                    count += 1;
+                    self.storage[i] = false;
+                }
+            }
+        }
+        count
+    }
+}
