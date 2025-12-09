fabf16b2621bd77bd1c14563c38b10b5427c6730
Nick Fagerlund <nick.fagerlund@gmail.com>
2025-12-09 09:07:20 -0800

    test 9-1

--- src/dispatch/day9.rs
+++ src/dispatch/day9.rs
@@ -8,11 +8,19 @@ pub fn part2(input: &str) -> Result<String, anyhow::Error> {
     Err(anyhow!("not implemented"))
 }
 
-const _EXAMPLE: &str = "";
+const _EXAMPLE: &str = "7,1
+11,1
+11,7
+9,7
+9,5
+2,5
+2,3
+7,3
+";
 
 #[test]
 fn part1_test() {
-    assert_eq!(part1(_EXAMPLE).expect("should ok"), "LOL".to_string());
+    assert_eq!(part1(_EXAMPLE).expect("should ok"), "50".to_string());
 }
 
 #[test]
