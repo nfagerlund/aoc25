87362c9a9159780a8e81b1f5dea9c149030e3c84
Nick Fagerlund <nick.fagerlund@gmail.com>
2025-12-10 09:51:47 -0800

    10-1 test

--- src/dispatch/day10.rs
+++ src/dispatch/day10.rs
@@ -1,5 +1,7 @@
 use anyhow::anyhow;
 
+/// Ignore `{joltage requirements}` and determine the fewest button presses
+/// needed to make the lights match the desired pattern.
 pub fn part1(input: &str) -> Result<String, anyhow::Error> {
     Err(anyhow!("not implemented"))
 }
@@ -8,11 +10,14 @@ pub fn part2(input: &str) -> Result<String, anyhow::Error> {
     Err(anyhow!("not implemented"))
 }
 
-const _EXAMPLE: &str = "";
+const _EXAMPLE: &str = "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
+";
 
 #[test]
 fn part1_test() {
-    assert_eq!(part1(_EXAMPLE).expect("should ok"), "LOL".to_string());
+    assert_eq!(part1(_EXAMPLE).expect("should ok"), "7".to_string());
 }
 
 #[test]
